02efe47730b312d77b36858b7e66a68b1f8b5f1a
Jay Meck <jmeck@p-r-i.org>
2019-11-21 14:59:03 -0500

    Committer: Jay Meck <jmeck@p-r-i.org>
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   todo-project/.babelrc
    	new file:   todo-project/.editorconfig
    	new file:   todo-project/.gitignore
    	new file:   todo-project/.postcssrc.js
    	new file:   todo-project/README.md
    	new file:   todo-project/build/build.js
    	new file:   todo-project/build/check-versions.js
    	new file:   todo-project/build/logo.png
    	new file:   todo-project/build/utils.js
    	new file:   todo-project/build/vue-loader.conf.js
    	new file:   todo-project/build/webpack.base.conf.js
    	new file:   todo-project/build/webpack.dev.conf.js
    	new file:   todo-project/build/webpack.prod.conf.js
    	new file:   todo-project/config/dev.env.js
    	new file:   todo-project/config/index.js
    	new file:   todo-project/config/prod.env.js
    	new file:   todo-project/index.html
    	new file:   todo-project/package-lock.json
    	new file:   todo-project/package.json
    	new file:   todo-project/src/App.vue
    	new file:   todo-project/src/assets/logo.png
    	new file:   todo-project/src/components/HelloWorld.vue
    	new file:   todo-project/src/components/Todo.vue
    	new file:   todo-project/src/main.js
    	new file:   todo-project/static/.gitkeep
    
    Changes not staged for commit:
    	deleted:    todo-app/.babelrc
    	deleted:    todo-app/.editorconfig
    	deleted:    todo-app/.eslintignore
    	deleted:    todo-app/.eslintrc.js
    	deleted:    todo-app/.gitignore
    	deleted:    todo-app/.postcssrc.js
    	deleted:    todo-app/README.md
    	deleted:    todo-app/build/build.js
    	deleted:    todo-app/build/check-versions.js
    	deleted:    todo-app/build/logo.png
    	deleted:    todo-app/build/utils.js
    	deleted:    todo-app/build/vue-loader.conf.js
    	deleted:    todo-app/build/webpack.base.conf.js
    	deleted:    todo-app/build/webpack.dev.conf.js
    	deleted:    todo-app/build/webpack.prod.conf.js
    	deleted:    todo-app/config/dev.env.js
    	deleted:    todo-app/config/index.js
    	deleted:    todo-app/config/prod.env.js
    	deleted:    todo-app/config/test.env.js
    	deleted:    todo-app/index.html
    	deleted:    todo-app/package-lock.json
    	deleted:    todo-app/package.json
    	deleted:    todo-app/src/App.vue
    	deleted:    todo-app/src/assets/logo.png
    	deleted:    todo-app/src/components/HelloWorld.vue
    	deleted:    todo-app/src/main.js
    	deleted:    todo-app/static/.gitkeep
    	deleted:    todo-app/test/unit/.eslintrc
    	deleted:    todo-app/test/unit/jest.conf.js
    	deleted:    todo-app/test/unit/setup.js
    	deleted:    todo-app/test/unit/specs/HelloWorld.spec.js
    
    Untracked files:
    	package-lock.json

---
+++ todo-project/src/components/Todo.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="well">
+                    <h2>Create a New Todo</h2>
+                    <form>
+                        <div class="form-group">
+                            <label for="todoitem">Todo Item</label>
+                            <input type="text" v-model="input" class="form-control" id="todoitem" placeholder="Todo Item" />
+                        </div>
+                        <button type="button" v-on:click="add()" class="btn btn-default">Create</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h2>List</h2>
+                <h3>Total number of Todos: {{totalTodos}}</h3>
+                <ul class="list-group">
+                    <li v-for="(todo, index) in todos" :key="index" class="list-group-item">
+                        {{ todo.id }}: {{ todo.data }}
+                        <button type="button" v-on:click="remove(todo.id)" class="btn btn-default">Delete</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todo",
+    data () {
+        return {
+            todos: [],
+            input: "",
+            id: 0
+        }
+    },
+    methods: {
+        add() {
+            var todo = {
+                id: this.id,
+                data: this.input
+            }
+            this.todos.push(todo);
+            this.id ++;
+            this.input = "";
+        },
+        remove (idToDelete) {
+            this.todos = this.todos.filter(ele => ele.id !== idToDelete);
+        }
+    },
+    computed: {
+        totalTodos (){
+            return this.todos.length;
+        }
+    }
+}
+</script>
